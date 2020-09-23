66d1fc2100dec8c2314268500d5c985f57ffd123
Martin Luna <martin.luna@possible.com>
2020-09-23 16:50:59 -0300

    test query params in axios to limit results num and filter

--- src/components/CitiesList.js
+++ src/components/CitiesList.js
@@ -5,10 +5,12 @@ class CitiesList extends React.Component {
   state = { cities: [] };
 
   componentDidMount() {
-    axios.get("http://localhost:3030/cities")
+    axios.get("http://localhost:3030/cities", {
+      params: { limit: 10, filter: "buenos" }
+    })
     .then((response) => {
       // console.log(response.data);
-      console.log(response.data.data);
+      // console.log(response.data.data.slice(0, 10));
       this.setState({ cities: response.data.data })
     });
   }
