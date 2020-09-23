63c53c0dc49c792ab5716cbf20085e848c6dd856
Martin Luna <martin.luna@possible.com>
2020-09-23 17:16:48 -0300

    refactor App to a Class component and move axios from citiesList

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,31 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 import CitiesList from "./CitiesList";
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-      <CitiesList />
-    </div>
-  )
+class App extends React.Component {
+  onSearchSubmit(term) {
+    console.log(term);
+  }
+
+  componentDidMount() {
+    axios.get("http://localhost:3030/cities", {
+      params: { limit: 10, filter: "buenos" }
+    })
+    .then((response) => {
+      // console.log(response);
+      this.setState({ cities: response.data.data })
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+        <CitiesList />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/CitiesList.js
+++ src/components/CitiesList.js
@@ -1,19 +1,7 @@
 import React from "react";
-import axios from "axios";
 
 class CitiesList extends React.Component {
   state = { cities: [] };
-
-  componentDidMount() {
-    axios.get("http://localhost:3030/cities", {
-      params: { limit: 10, filter: "buenos" }
-    })
-    .then((response) => {
-      // console.log(response.data);
-      // console.log(response.data.data.slice(0, 10));
-      this.setState({ cities: response.data.data })
-    });
-  }
 
   render() {
     return (
