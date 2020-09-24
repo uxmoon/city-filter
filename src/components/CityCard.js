003369539c15f88c61804cf16f8a34ac436f89a3
Martin Luna <martin.luna@possible.com>
2020-09-24 19:27:26 -0300

    use destructuring with city props

--- src/components/CityCard.js
+++ src/components/CityCard.js
@@ -1,16 +1,14 @@
 import React from "react";
 
-class CityCard extends React.Component {
-  render() {
-    return (
+const CityCard = ({ city }) => {
+  return (
+    <div>
+      {city.name}
       <div>
-        {this.props.city.name}
-        <div>
-        {this.props.city.subcountry}, {this.props.city.country}
-        </div>
+        {city.subcountry}, {city.country}
       </div>
-    )
-  }
-}
+    </div>
+  );
+};
 
 export default CityCard;
