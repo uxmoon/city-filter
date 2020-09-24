d722b2dbc804010e1c3a0f6ca11bc4614870b59f
Martin Luna <martin.luna@possible.com>
2020-09-24 19:27:11 -0300

    rename variable to avoid confusion

--- src/components/CitiesList.js
+++ src/components/CitiesList.js
@@ -2,10 +2,10 @@ import React from "react";
 import CityCard from "./CityCard";
 
 const CitiesList = (props) => {
-  const cities = props.cities.map((city) => {
+  const renderedList = props.cities.map((city) => {
     return <CityCard key={city.geonameid} city={city} />
   })
-  return <div>{cities}</div>;
+  return <div>{renderedList}</div>;
 }
 
 export default CitiesList;
