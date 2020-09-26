f69d6d0bfa517c9fe2459bcb34020a1a81f1c726
Martin Luna <uxmoon@gmail.com>
2020-09-26 12:04:52 -0300

    added useState and initialize with empty string, assign value and input event handler

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Search = () => {
+  /* init state */
+  const [term, setTerm] = useState('');
+
   return (
     <div>
       <div className="ui form">
         <div className="field">
           <label>Search city</label>
-          <input className="input" />
+          <input
+            value={term}
+            onChange={(event) => setTerm(event.target.value)}
+            className="input"
+          />
         </div>
       </div>
     </div>
-  )
+  );
 };
 
 export default Search;
