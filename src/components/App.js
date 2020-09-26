273c676d615adc6621ed6b61ef57f8cf552f5c61
Martin Luna <uxmoon@gmail.com>
2020-09-26 13:01:30 -0300

    updated design layout

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,10 @@ import Search from './Search';
 export default () => {
   return (
     <div className="ui container">
+      <h1 className="ui header">
+        <i className="icon map outline"></i>
+        <div className="content">Find your favorites cities</div>
+      </h1>
       <Search />
     </div>
   );
--- src/components/Search.js
+++ src/components/Search.js
@@ -33,9 +33,11 @@ const Search = () => {
     };
   }, [term]);
 
+  /* Results list */
   const renderedResults = results.map((result) => {
     return (
       <div className="item" key={result.geonameid}>
+        <i className="map marker icon"></i>
         <div className="content">
           <div className="header">{result.name}</div>
           {result.subcountry}, {result.country}
@@ -48,15 +50,18 @@ const Search = () => {
     <div>
       <div className="ui form">
         <div className="field">
-          <label>Search city</label>
-          <input
-            value={term}
-            onChange={(event) => setTerm(event.target.value)}
-            className="input"
-          />
+          <div className="ui large icon input">
+            <input
+              value={term}
+              onChange={(event) => setTerm(event.target.value)}
+              className="input"
+              placeholder="Search..."
+            />
+            <i className="search icon"></i>
+          </div>
         </div>
       </div>
-      <div className="ui celled list">{renderedResults}</div>
+      <div className="ui selection celled list">{renderedResults}</div>
     </div>
   );
 };
