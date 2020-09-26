81b6d85915543ade038db627009cb432ef07ba08
Martin Luna <uxmoon@gmail.com>
2020-09-26 12:00:25 -0300

    Alternative approach, use hooks with function components

--- src/components/App.js
+++ src/components/App.js
@@ -1,40 +1,10 @@
-import React from "react";
-import SearchBar from "./SearchBar";
-import CitiesList from "./CitiesList";
-import API from "./api.js";
-import CitySelection from "./CitySelection";
+import React from 'react';
+import Search from './Search';
 
-class App extends React.Component {
-  state = { cities: [], selectedCity: [] };
-
-  onSearchSubmit = async (term) => {
-    const response = await API.get("/cities", {
-      params: {
-        filter: term,
-      },
-    });
-
-    // console.log(response.data.data);
-    this.setState({ cities: response.data.data });
-  };
-
-  onCitySelect = (city) => {
-    // console.log("Selection", city.name)
-    this.setState({ selectedCity: this.state.selectedCity.concat(city) });
-  };
-
-  render() {
-    return (
-      <div className="ui container">
-        <CitySelection cities={this.state.selectedCity} />
-        <SearchBar onSubmit={this.onSearchSubmit} />
-        <CitiesList
-          cities={this.state.cities}
-          onCitySelect={this.onCitySelect}
-        />
-      </div>
-    );
-  }
-}
-
-export default App;
+export default () => {
+  return (
+    <div>
+      <Search />
+    </div>
+  );
+};
